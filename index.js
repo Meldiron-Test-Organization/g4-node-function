5f5847cc164832a1fded68ff3be602e3f0e44ef4
Matej Bačo <matejbaco2000@gmail.com>
2023-05-22 16:19:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 module.exports = async (context) => {
-  return context.res.send('<h1>Hello! 👋</h1>', 200, {
+   const code = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+      <h1>Hello! 👋</h1>
+</body>
+</html>`;
+  return context.res.send(code, 200, {
     'content-type': 'text/html'
   });
 };
